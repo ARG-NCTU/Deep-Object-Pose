013baf1bf2e788cb43560dca8bc437d338877732
yutingk <yutingk.ee11@nycu.eu.tw>
2023-05-12 19:00:54 +0800

    add script for model and dataset

---
+++ download_dataset.sh
@@ -0,0 +1,8 @@
+############################################
+# dataset name: drone_view_10m.zip         #
+#               medical_cup.zip            #
+#               LIVALO.zip                 #
+#               TYLENOL.zip                #
+#               water_cup.zip              #
+############################################
+wget ftp://arg.lab.nycu.edu.tw/arg-projectfile-download/Deep-Object-Pose/dataset/drone_view_10m.zip
---
+++ download_model.sh
@@ -0,0 +1,8 @@
+############################################
+# dataset name: drone_view_10m.pth         #
+#               medical_cup.pth            #
+#               LIVALO.pth                 #
+#               TYLENOL.pth                #
+#               water_cup.pth              #
+############################################
+wget ftp://arg.lab.nycu.edu.tw/arg-projectfile-download/Deep-Object-Pose/weights/drone_wamv_10m.pth
