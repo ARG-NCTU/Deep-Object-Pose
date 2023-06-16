f1902941cf1bb9fa1c84d477e0344818b152be5b
yutingk <yutingk.ee11@nycu.eu.tw>
2023-06-16 14:36:24 +0800

    update download script

--- download_model.sh
+++ download_model.sh
@@ -1,8 +1,8 @@
 ############################################
-# dataset name: drone_view_10m.pth         #
+# dataset name: net_wamv_27.pth            #
 #               medical_cup.pth            #
 #               LIVALO.pth                 #
 #               TYLENOL.pth                #
 #               water_cup.pth              #
 ############################################
-wget ftp://arg.lab.nycu.edu.tw/arg-projectfile-download/Deep-Object-Pose/weights/drone_wamv_10m.pth
+wget ftp://arg.lab.nycu.edu.tw/arg-projectfile-download/Deep-Object-Pose/weights/net_wamv_27.pth
